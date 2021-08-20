54fc9f185c3d2d0771afdec2b4c631d29614ae64
Dogan <doganozgur034@gmail.com>
2021-08-20 17:58:51 +0300

    os module, sync and async fs

--- 8-module-os.js
+++ 8-module-os.js
@@ -7,6 +7,7 @@ const user = os.userInfo()
 // const uptime = os.uptime()
 console.log(`The system uptime is ${os.uptime} seconds`);
 
+
 const currentOS = {
     name: os.type(),
     release: os.release(),
