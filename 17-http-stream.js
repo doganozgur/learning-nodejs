0789907763433ecc06abd2fa308fbf1b326d6b19
Dogan <doganozgur034@gmail.com>
2021-08-24 16:35:03 +0300

    event loop, async patterns, event emitter and streams

---
+++ 17-http-stream.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const { readFileSync, createReadStream } = require('fs')
+
+http.createServer((req, res) => {
+   //  const text = readFileSync('./content/big.txt', 'utf8')
+   //  res.end(text)
+
+   const fileStream =  createReadStream('./content/big.txt', 'utf8');
+   fileStream.on('open', () => {
+       fileStream.pipe(res)
+   })
+   fileStream.on('error', (err) => {
+       res.end(err)
+   })
+
+}).listen(5000)
